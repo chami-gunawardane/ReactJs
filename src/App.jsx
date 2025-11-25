8590fb4641f9383b1843714de42404dc223cf707
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-26 00:17:33 +0530

    Properties-(Props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import './App.css'
 
-//Card Component
-const Card = () => {
+// Card Component with props
+const Card = ({ title, rating, isAvailable }) => {
   return (
     <div>
-     <h2>Card component</h2>
+      <h2>{title}</h2>
+
+      {/* New numeric prop */}
+      <p>Rating: {rating}</p>
+
+      {/* New boolean prop */}
+      <p>Available: {isAvailable ? "Yes" : "No"}</p>
     </div>
   )
 }
 
-//App Component
+
+// App Component
 const App = () => {
   return (
-    // React Fragment
     <>
-        <h2>Functional Arrow Component</h2>
-        <Card /> {/* Using card component inside App component */}
+      <h2>Functional Arrow Component</h2>
+
+      {/* Using card component inside App component */}
+      <Card title="Star Wars" rating={9.5} isAvailable={true} />
+      <Card title="Avatar" rating={8.2} isAvailable={false} />
+      <Card title="The Lion King" rating={9.0} isAvailable={true} />
     </>
   )
 }
+
 
 export default App
