a4790cf9512d3d9d5a719d026c3e6f27cbfde159
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-27 00:10:48 +0530

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css'
 // Card Component with props
 const Card = ({ title, rating, isAvailable }) => {
   return (
-    <div>
+    //inline styling
+    <div className='card' 
+    // style={{
+    //   border : '1px solid #4b5362',
+    //   padding : '20px',
+    //   margin : '10px',
+    //   backgroundColor : '#31363f',
+    //   borderRadius : '10px',
+    //   minHeight : '100px',
+    // }}
+    >
       <h2>{title}</h2>
 
       {/* New numeric prop */}
@@ -22,10 +32,12 @@ const App = () => {
     <>
       <h2>Functional Arrow Component</h2>
 
+      <div className='card-container'>
       {/* Using card component inside App component */}
       <Card title="Star Wars" rating={9.5} isAvailable={true} />
       <Card title="Avatar" rating={8.2} isAvailable={false} />
       <Card title="The Lion King" rating={9.0} isAvailable={true} />
+      </div>
     </>
   )
 }
